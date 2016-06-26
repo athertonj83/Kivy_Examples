7f7025efe4f9a1b8461978d66f9029c1990db09a
Jennifer Atherton <jenatherton03@gmail.com>
2016-06-26 19:53:06 +0100

    Commiting changes to make applicable for python3 (update to python2)

--- Coding_with_James.py
+++ Coding_with_James.py
@@ -1,19 +1,18 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python3
 
 #this creates fizz buzz woof
 
 # Dictionary of associations
 assoc={3:"Fizz", 5:"Buzz", 7:"Woof"}
 
-
-for i in range(1,20):
+for i in range(20):
 	word=""
 	if i%3==0:
-		word="Fizz"		
+		word="Fizz"
 	if i%5==0:
 		word=word+"Buzz"
 	if i%7==0:
 		word=word+"Woof"
 	if word=="":
 		word=str(i)
-	print word
+	print(word)
