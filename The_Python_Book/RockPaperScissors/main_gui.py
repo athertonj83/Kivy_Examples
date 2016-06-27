82354d122799a1eb09efb9ae426707b153fd4e1c
Jennifer Atherton <jenatherton03@gmail.com>
2016-06-27 16:43:08 +0100

    Updating main gui to work with python3 (still requires some working)

--- The_Python_Book/RockPaperScissors/main_gui.py
+++ The_Python_Book/RockPaperScissors/main_gui.py
@@ -1,28 +1,29 @@
 #!/usr/bin/env python3
 
 # Creates the overall interface to access the game
+from tkinter import *
+from tkinter.ttk import *
+import random
+import rps_gui_python3
 
-from Tkinter import *
 
-import rps_2_gui
+main=Tk()
+main.title("Jen's games")
 
-root=Tk()
-root.title("Jen's games")
-
-mainframe=Frame(root,height=200,width=500)
+mainframe=Frame(main,height=200,width=500)
 mainframe.pack_propagate(0)
 mainframe.pack(padx=5,pady=5)
 
 intro=Label(mainframe,text="""Welcome to Jen's Games. Please make your (RPS) choice.""")
 intro.pack(side=TOP)
 
-rps_button=Button(mainframe,text="Rock,Paper,Scissors", command=rps_2_gui.gui)
+rps_button=Button(mainframe,text="Rock,Paper,Scissors", command=rps_gui_python3.gui)
 rps_button.pack()
 
 test_button=Button(mainframe,text="Test Button")
 test_button.pack()
 
-exit_button=Button(mainframe,text="Quit", command=root.destroy)
+exit_button=Button(mainframe,text="Quit", command=main.destroy)
 exit_button.pack(side=BOTTOM)
 
-root.mainloop()
+main.mainloop()
