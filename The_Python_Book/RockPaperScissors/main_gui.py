3d92533f9a48fc0690c1d2cb756ecb88cd601cfa
Jennifer Atherton <jenatherton03@gmail.com>
2016-06-28 10:24:37 +0100

    Creating 2 version of rps gui - one to work with the main gui and one to work independently

--- The_Python_Book/RockPaperScissors/main_gui.py
+++ The_Python_Book/RockPaperScissors/main_gui.py
@@ -4,7 +4,7 @@
 from tkinter import *
 from tkinter.ttk import *
 import random
-import rps_gui_python3
+import rps_gui_python3_withmain
 
 main=Tk()
 main.title("Jen's games")
@@ -16,7 +16,7 @@ mainframe.pack(padx=5,pady=5)
 intro=Label(mainframe,text="""Welcome to Jen's Games. Please make your (RPS) choice.""")
 intro.pack(side=TOP)
 
-rps_button=Button(mainframe,text="Rock,Paper,Scissors", command=rps_gui_python3.gui)
+rps_button=Button(mainframe,text="Rock,Paper,Scissors", command=rps_gui_python3_withmain.gui)
 rps_button.pack()
 
 test_button=Button(mainframe,text="Test Button")
--- The_Python_Book/RockPaperScissors/rps_gui_python3_withmain.py
+++ The_Python_Book/RockPaperScissors/rps_gui_python3_withmain.py
@@ -1,14 +1,14 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python3
 
 #this creates a GUI for the rock, paper, scissors from the Python book
 
-from Tkinter import *
-from ttk import *
+from tkinter import *
+from tkinter.ttk import *
 import random
 
 
 def gui():
-	
+
 	rock=1
 	paper=2
 	scissors=3
@@ -49,52 +49,41 @@ def gui():
 				new_score=computer_score.get()
 				new_score+=1
 				computer_score.set(new_score)
-			
-	rps_window=Toplevel()
-	rps_window.title("Rock, Paper, Scissors")
-	
+
+	#Using tkdocs to help!
+	root=Toplevel()
+	root.title("Rock, Paper, Scissors")
+
 	player_choice=IntVar()
 	computer_choice=StringVar()
 	result_set=StringVar()
 	player_choice.set(1)
 	player_score=IntVar()
 	computer_score=IntVar()
-	
-	rps_frame=Frame(rps_window,padding='3 3 12 12', width=300)
+
+	rps_frame=Frame(root,padding='3 3 12 12', width=300)
 	rps_frame.grid(column=0,row=0,sticky=(N,W,E,S))
 	rps_frame.columnconfigure(0,weight=1)
 	rps_frame.rowconfigure(0,weight=1)
-	
+
 	Label(rps_frame,text='Player').grid(column=1,row=1,sticky=W)
 	Radiobutton(rps_frame,text='Rock',variable=player_choice,value=1).grid(column=1,row=2,sticky=W)
 	Radiobutton(rps_frame,text='Paper',variable=player_choice,value=2).grid(column=1,row=3,sticky=W)
 	Radiobutton(rps_frame,text='Scissors',variable=player_choice,value=3).grid(column=1,row=4,sticky=W)
-	
+
 	Label(rps_frame, text="Computer").grid(column=3,row=1,sticky=W)
 	Label(rps_frame, textvariable=computer_choice).grid(column=3,row=3,sticky=W)
-	
+
 	Button(rps_frame,text="Play",command=start).grid(column=2,row=2)
-	
+
 	Label(rps_frame, text="Score").grid(column=1,row=5,sticky=W)
 	Label(rps_frame, textvariable=player_score).grid(column=1,row=6,sticky=W)
-	
+
 	Label(rps_frame, text="Score").grid(column=3,row=5,sticky=W)
 	Label(rps_frame, textvariable=computer_score).grid(column=3,row=6,sticky=W)
-	
-	Label(rps_frame,textvariable=result_set).grid(column=2,row=7)	
-	
-	
+
+	Label(rps_frame,textvariable=result_set).grid(column=2,row=7)
+
+
 if __name__=='__main__':
-		gui()
-		
-		
-		
-		
-		
-
-
-
-
-
-
-
+	gui()
