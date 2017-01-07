77140d4be4fb475a0c35f3559fd8dd95e21d46e1
Jennifer Atherton <jen_atherton@me.com>
2017-01-07 20:25:19 +0000

    Final changes to R Regex

--- 070117_Regex.R
+++ 070117_Regex.R
@@ -2,19 +2,20 @@ library(stringr)
 library(stringi)
 
 PDF_examples
+tmp=str_match(PDF_examples$X1,"(\\d+)\\s+(\\D+)\\s+(-?(\\d{1,3}(,\\d{3})*(.\\d{2})?))\\s+(!!!)?\\s+(-?(\\d{1,3}(,\\d{3})*(.\\d{2})?))\\s+(\\d{2}/\\d{2}/\\d{4})\\s+(\\*\\*\\*)?\\s+(\\S+)\\s+(\\d+\\.\\d{4})\\s+(\\S+)\\s+(-?(\\d{1,3}(,\\d{3})*(.\\d{2})?))")
+tmp
 
 id<-tmp[,2]
 company_name<-tmp[,3]
 first_neg<-tmp[,4]
-exc<-tmp[,8]
-
-tmp5=str_match(PDF_examples$X1,"(\\d+)\\s+(\\D+)\\s+(-?(\\d{1,3}(,\\d{3})*(.\\d{2})?))\\s+(!!!)?\\s+(-?(\\d{1,3}(,\\d{3})*(.\\d{2})?))\\s+(\\d{2}/\\d{2}/\\d{4})")
-tmp5
-
-tmp6=str_match(PDF_examples$X1,"(\\d+)\\s+(\\D+)\\s+(-?(\\d{1,3}(,\\d{3})*(.\\d{2})?))\\s+(!!!)?\\s+(-?(\\d{1,3}(,\\d{3})*(.\\d{2})?))\\s+(\\d{2}/\\d{2}/\\d{4})\\s+(\\*\\*\\*)?\\s+(\\")
-tmp6
-
-
+exclam<-tmp[,8]
+sec_neg<-tmp[,10]
+expiry_date<-tmp[,13]
+stars<-tmp[,14]
+after_stars<-tmp[,15]
+interest_rate<-tmp[,16]
+after_IR<-tmp[,17]
+last_neg<-tmp[,18]
 
 
 
